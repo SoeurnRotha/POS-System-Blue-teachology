dfb0143dbbb8c56e371829f824d0869937570299
SoeurnRotha <kl195070@gmail.com>
2023-03-02 08:58:22 +0700

    hello wolrd

---
+++ app/src/main/java/MyAdapter/pos_adapter/Pos_category_adapter_show_list_view.java
@@ -0,0 +1,58 @@
+package MyAdapter.pos_adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.bluesystemwithroomdatabase.R;
+
+import java.util.List;
+
+import Model.CategoryTable;
+
+public class Pos_category_adapter_show_list_view extends RecyclerView.Adapter<Pos_category_adapter_show_list_view.ViewCategoryPos>{
+
+    List<CategoryTable> categoryTableList;
+
+    public Pos_category_adapter_show_list_view(List<CategoryTable> categoryTableList) {
+        this.categoryTableList = categoryTableList;
+    }
+
+    @NonNull
+    @Override
+    public ViewCategoryPos onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view =
+                LayoutInflater
+                        .from(parent.getContext())
+                        .inflate(R.layout.single_list_item_category_show_in_pos, parent, false);
+
+        return new ViewCategoryPos(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewCategoryPos holder, int position) {
+        holder.category_eng.setText(categoryTableList.get(position).getCategoryname_Eng());
+
+        holder.category_kh.setText(categoryTableList.get(position).getCategoryname_kh());
+    }
+
+    @Override
+    public int getItemCount() {
+        return categoryTableList.size();
+    }
+
+    public class ViewCategoryPos extends RecyclerView.ViewHolder{
+
+        TextView category_eng, category_kh;
+        public ViewCategoryPos(@NonNull View itemView) {
+            super(itemView);
+
+            category_eng = itemView.findViewById(R.id.pos_category_eng);
+            category_kh = itemView.findViewById(R.id.pos_category_kh);
+        }
+    }
+}
--- app/src/main/java/com/example/bluesystemwithroomdatabase/Pos/Pos.java
+++ app/src/main/java/com/example/bluesystemwithroomdatabase/Pos/Pos.java
@@ -1,16 +1,39 @@
 package com.example.bluesystemwithroomdatabase.Pos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 
 import com.example.bluesystemwithroomdatabase.R;
+import com.example.bluesystemwithroomdatabase.databinding.ActivityPosBinding;
+
+import java.util.List;
+
+import Dao.BlueTeachnology_Dao;
+import Model.CategoryTable;
+import MyAdapter.pos_adapter.Pos_category_adapter_show_list_view;
+import Mydatabase.BlueTeachnology_Database;
 
 public class Pos extends AppCompatActivity {
 
+
+    ActivityPosBinding binding;
+
+    BlueTeachnology_Dao blueTeachnology_dao;
+    List<CategoryTable> categoryTableList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pos);
+
+        binding = ActivityPosBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        blueTeachnology_dao = BlueTeachnology_Database.getInstance(getApplicationContext()).blueTeachnology_dao();
+
+        categoryTableList = blueTeachnology_dao.getAllCateroy();
+
+        Pos_category_adapter_show_list_view adapter_show_list_category = new Pos_category_adapter_show_list_view(categoryTableList);
+        binding.listCategoryPos.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+        binding.listCategoryPos.setAdapter(adapter_show_list_category);
     }
 }
